189b92388c06cefaa05de11d127e0bb361a87674
michaeljstevens <michaeljstevens@zoho.com>
2016-11-15 23:11:02 -0800

    added get and post actions

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express();
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+  console.log("hello");
+});
